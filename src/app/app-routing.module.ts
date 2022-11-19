4c3df40eb41ce7c563daf4434de7fcce32224414
rajkesh117 <yadavrajkesh9923@gmail.com>
2022-11-19 20:43:06 +0530

    initial design setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingpageComponent } from './model/landingpage/landingpage.component';
+import { HomepageComponent } from './model/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingpageComponent },
+  { path: 'HomepageComponent', component: HomepageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
