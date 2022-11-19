07f984489d4e7e936cd25f4e959a52e4a981c11e
rajkesh117 <yadavrajkesh9923@gmail.com>
2022-11-19 13:55:58 +0530

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingpageComponent } from './model/landingpage/landingpage.component';
+import { HomepageComponent } from './model/homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingpageComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
